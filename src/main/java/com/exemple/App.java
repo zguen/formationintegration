701c7ecff7d77588d8b2473708ce59d7a53faaff
zguen <94364514+zguen@users.noreply.github.com>
2026-03-25 16:38:01 +0100

    Update App.java

--- src/main/java/com/exemple/App.java
+++ src/main/java/com/exemple/App.java
@@ -3,7 +3,7 @@ package com.exemple;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Bonjour, Jenkins (encore) ! Ce projet est compilé avec succès.");
+        System.out.println("Bonjour, Jenkins (encore) ! Ce projet est compilé avec succès. Ratatatatata");
     }
 
     public String getMessage() {
